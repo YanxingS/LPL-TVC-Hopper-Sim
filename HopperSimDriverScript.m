cd9d541855d7f69a69deb9359ef36f292469a0f0
YanxingS <yanxings@usc.edu>
2025-01-18 12:19:29 -0800

    Driver Script
    Included driver script to hopper sim

---
+++ HopperSimDriverScript.m
@@ -0,0 +1,35 @@
+clc; clear all; close all;
+
+%% Hopper Simulation Driver Script
+% Spring 2025
+% University of Southern California, Viterbi School of Engineering
+% Liquid Propulsion Lab
+% TVC: Enson Su
+
+%% Properties of Hopper
+
+% all units in imperial, ISP
+
+mf = 600; % mass of fuselage in lb
+Df = 12/12; % diameter of fuselage in feet
+Hf = 96/12; % length of fuselage in feet
+
+me = 110; % mass of engine
+De = 5/12; % diameter of fueslage in feet
+He = 20/12; % length of cone in feet
+
+% distance from cg to centroid
+xf = (96/2)/12; % in feet
+xe = -(20-20/4)/12; % in feet
+
+%cg location
+
+x_cg = ((mf*xf)+(me*xe))/(mf+me);
+
+% MOI matrix cal
+
+Ixx = 0.5*mf*(Df/2)^2+0.5*me*(De/2)^2;
+Iyy = (1/12)*mf*(3*(Df/2)^2+96^2)+(1/4)*me*((De/2)^2+2*(He)^2)+(mf*(xf-x_cg))^2+(me*(xe-x_cg))^2;
+Izz = Iyy;
+
+I_matrix = [Ixx 0 0; 0 Iyy 0; 0 0 Izz]; % MOI matrix about principal axis
